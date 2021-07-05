feb691cdc31eb9c1131cc21893ec289236e7e874
Marvin <marvin.hansen@gmail.com>
2021-07-05 17:45:25 +0800

    Removed some dead code

--- api/v1/sdk_utils.go
+++ api/v1/sdk_utils.go
@@ -12,20 +12,3 @@ func (s SDKImpl) getWSConfig() (wsCfg *types.WsConfig) {
 	}
 	return wsCfg
 }
-
-func (s SDKImpl) getHello(dataTypes, symbolIDs, assetIDs, periodIDs [25]string) (hello types.Hello) {
-
-	hello = types.Hello{
-		Type:                         "hello",
-		Api_key:                      s.config.ApiKey,
-		Heartbeat:                    s.config.Heartbeat,
-		Subscribe_data_type:          dataTypes,
-		Subscribe_filter_symbol_id:   symbolIDs,
-		Subscribe_filter_asset_id:    assetIDs,
-		Subscribe_filter_period_id:   periodIDs,
-		Subscribe_filter_exchange_id: s.config.Exchanges,
-	}
-
-	return hello
-
-}
