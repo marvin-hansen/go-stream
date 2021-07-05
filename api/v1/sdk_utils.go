425021c7d6ce24b4e072d650b22d9e7d9ea99599
Marvin <marvin.hansen@gmail.com>
2021-07-05 13:27:00 +0800

    Removed dead code

--- api/v1/sdk_utils.go
+++ api/v1/sdk_utils.go
@@ -13,23 +13,17 @@ func (s SDKImpl) getWSConfig() (wsCfg *types.WsConfig) {
 	return wsCfg
 }
 
-func (s SDKImpl) getHello(dataTypes, assetIds []string) (hello types.Hello) {
-	//var dataTypes = make([]string, 10)
-	//dataTypes[0] = "ohlcv"
-	//var assetIds = make([]string, 10)
-	//assetIds[0] = "BTC"
+func (s SDKImpl) getHello(dataTypes, symbolIDs, assetIDs, periodIDs []string) (hello types.Hello) {
 
 	hello = types.Hello{
-		Type:                                    "hello",
-		Api_key:                                 s.config.ApiKey,
-		Heartbeat:                               s.config.Heartbeat,
-		Subscribe_data_type:                     dataTypes,
-		Subscribe_filter_symbol_id:              nil,
-		Subscribe_filter_asset_id:               assetIds,
-		Subscribe_filter_period_id:              nil,
-		Subscribe_filter_exchange_id:            nil,
-		Subscribe_update_limit_ms_quote:         0,
-		Subscribe_update_limit_ms_book_snapshot: 0,
+		Type:                         "hello",
+		Api_key:                      s.config.ApiKey,
+		Heartbeat:                    s.config.Heartbeat,
+		Subscribe_data_type:          dataTypes,
+		Subscribe_filter_symbol_id:   symbolIDs,
+		Subscribe_filter_asset_id:    assetIDs,
+		Subscribe_filter_period_id:   periodIDs,
+		Subscribe_filter_exchange_id: s.config.Exchanges,
 	}
 
 	return hello
