5377aeec0019aa4584e57fb5c16e8ddde5341b3c
Marvin <marvin.hansen@gmail.com>
2021-07-06 10:12:56 +0800

    Added some documentation

--- api/utils.go
+++ api/utils.go
@@ -7,26 +7,32 @@ import (
 )
 
 func validateApiKey(apiKey string) {
+
+	// Check if key empty
 	if apiKey == "" {
-		printHeaader("API key is empty!", "Add a valid API key to main!")
+		printErrorMessage("API key is empty!", "Add a valid API key to main!")
 		panic("Invalid API KEY - Abort!")
 	}
 
+	// Check for test key
+	if strings.Contains(apiKey, "SAMPLE-KEY") || strings.Contains(apiKey, "SAMPLE") || strings.Contains(apiKey, "TEST") {
+		printErrorMessage("Inactive example key detected!", "Add your own valid API key to main!")
+		panic("Invalid API KEY - Abort!")
+	}
+
+	// Check for copy & paste error. Test keys should be 36 characters long!
+	// It's unclear to me if key length is fixed or dynamic, thus 21 seems to be the safest option.
 	if len(apiKey) <= 21 {
-		printHeaader("API key is too short. Should be 30 or more characters long!", "Add a valid API key to main!")
-		panic("Invalid API KEY - Abort!")
-	}
-
-	if strings.Contains(apiKey, "SAMPLE-KEY") {
-		printHeaader("Inactive sample key detected!", "Add your own valid API key to main!")
+		printErrorMessage("API key is too short. Should be 30 or more characters long!", "Add a valid API key to main!")
 		panic("Invalid API KEY - Abort!")
 	}
 }
 
-func printHeaader(problem, solution string) {
+func printErrorMessage(problem, solution string) {
 	println()
-	println("Problem: ", problem)
+	println("Problem : ", problem)
 	println("Solution: ", solution)
+	println("Web Link:  https://www.coinapi.io/pricing?apikey")
 	println()
 }
 
